39f08ed2ccc8363849e1e1eb09cf130788a935ee
Alasdair Collinson <Alasdair.Collinson@senacor.com>
2019-07-04 19:43:29 +0200

    Fixed prefix

--- tomono.sh
+++ tomono.sh
@@ -4,13 +4,13 @@ set -euxo pipefail
 #github_org=lgtm
 #my_repos="general client nginx server worker"
 my_repos="general"
-src_dir=./
+#src_dir=./
 git checkout -b monorepo-migration
 
 for repo in $(echo $my_repos); do
   git remote add $repo file:///Users/acollinson/workspaces/workspace-schulungen/docker-and-kubernetes/114-Checkpoint/$repo
   git fetch $repo
-  git read-tree --prefix=$src_dir/$repo -u $repo
+  git read-tree --prefix=$repo -u $repo
   git add $src_dir/$repo
   git commit -m "Migrated $repo to $src_dir/$repo"
 done
