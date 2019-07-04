23c02695cada8a8b734843de807fd4ce10dfeaae
Alasdair Collinson <Alasdair.Collinson@senacor.com>
2019-07-04 19:45:32 +0200

    Removed src dir

--- tomono.sh
+++ tomono.sh
@@ -11,8 +11,8 @@ for repo in $(echo $my_repos); do
   git remote add $repo file:///Users/acollinson/workspaces/workspace-schulungen/docker-and-kubernetes/114-Checkpoint/$repo
   git fetch $repo
   git read-tree --prefix=$repo -u $repo/master
-  git add $src_dir/$repo
-  git commit -m "Migrated $repo to $src_dir/$repo"
+  git add $repo
+  git commit -m "Migrated $repo to $repo"
 done
 
 git push -u origin HEAD
